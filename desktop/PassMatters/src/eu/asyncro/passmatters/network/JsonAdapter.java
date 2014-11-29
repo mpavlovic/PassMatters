57ad1b7749fc0dd7c3e621ec1a96b1acf35692c4
Milan Pavlović <bluraymimi@gmail.com>
2014-11-29 19:23:35 +0100

    Implemented JsonAdapter class for working with json (used external library).

---
+++ desktop/PassMatters/src/eu/asyncro/passmatters/network/JsonAdapter.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.asyncro.passmatters.network;
+
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Milan
+ */
+public class JsonAdapter {
+    
+    private static final String END = "\n";
+    private static final String CODE = "code";
+    private static final String MESSAGE = "message";
+        
+    public static String getToken(String jsonString) {
+        jsonString = jsonString.replace(END,"");
+        JSONObject object = new JSONObject(jsonString);
+        if(object.getInt(CODE) == 1 && object.getString(MESSAGE).equals("Success")) {
+            return object.getString("token");
+        }
+        else return null;
+    }
+    
+    public static String getAuthJsonString(String token) {
+        JSONObject object = new JSONObject();
+        object.put("step", 1);
+        object.put("token", token);
+        return object.toString();
+    }
+}

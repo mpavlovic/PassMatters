d6bbbb310e943c9493072091db9091475a30ea1d
ahuskanovic <alen.huskanovic@asseco-see.hr>
2015-01-11 00:16:41 +0100

    Security  lockreceiver added

--- mobile/PassMatters/app/src/main/java/eu/asyncro/passmatters/fragments/FragmentAccounts.java
+++ mobile/PassMatters/app/src/main/java/eu/asyncro/passmatters/fragments/FragmentAccounts.java
@@ -11,26 +11,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 import eu.asyncro.passmatters.R;
+import eu.asyncro.passmatters.activitys.ActivityMain;
+import eu.asyncro.passmatters.adapters.ThreeDListAdapter;
 import eu.asyncro.passmatters.controllers.ControllerAccount;
 import eu.asyncro.passmatters.controllers.ControllerAccounts;
 import eu.asyncro.passmatters.controllers.ControllerLogOut;
 import eu.asyncro.passmatters.controllers.ManagerSession;
-import eu.asyncro.passmatters.adapters.ThreeDListAdapter;
-import eu.asyncro.passmatters.widgets.ThreeDListItemView;
 import eu.asyncro.passmatters.data.DataAccount;
 import eu.asyncro.passmatters.data.responses.ResponseAccount;
 import eu.asyncro.passmatters.data.responses.ResponseAccounts;
 import eu.asyncro.passmatters.data.responses.ResponseLogOut;
 import eu.asyncro.passmatters.interfaces.OnAccountSendedListener;
+import eu.asyncro.passmatters.interfaces.OnLockListener;
 import eu.asyncro.passmatters.interfaces.OnLogOutListener;
+import eu.asyncro.passmatters.receivers.LockReceiver;
 import eu.asyncro.passmatters.widgets.HighlightedViewContainer;
+import eu.asyncro.passmatters.widgets.ThreeDListItemView;
 import eu.asyncro.passmatters.widgets.ThreeDListView;
 import retrofit.RetrofitError;
 
 /**
  * Created by ahuskano on 11/8/2014.
  */
-public class FragmentAccounts extends BaseFragment implements OnDataReadListener, OnAccountSendedListener, OnErrorListener, OnLogOutListener, AdapterView.OnItemClickListener {
+public class FragmentAccounts extends BaseFragment implements OnDataReadListener, OnAccountSendedListener, OnErrorListener, OnLogOutListener, AdapterView.OnItemClickListener, OnLockListener {
 
     private ControllerAccounts controllerAccounts;
     private ControllerAccount controllerAccount;
@@ -62,7 +65,7 @@ public class FragmentAccounts extends BaseFragment implements OnDataReadListener
         adapter = new ThreeDListAdapter(getActivity());
         listView.setOnItemClickListener(this);
         listView.setAdapter(adapter);
-
+        ((ActivityMain) getActivity()).setLockReceiver(new LockReceiver(getActivity(),this));
         initThreeDList();
     }
 
@@ -100,15 +103,16 @@ public class FragmentAccounts extends BaseFragment implements OnDataReadListener
     public void onLogOut(LightResponse response) {
         ResponseLogOut logOutResponse = (ResponseLogOut) response;
         if (response.getCode() == getResources().getInteger(R.integer.success_code)) {
-            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, new FragmentSignIn()).commit();
-
+            onLock();
         }
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        DataAccount data = (DataAccount) adapter.getItem(i-1);
-        controllerAccount.setAccount(ManagerSession.getToken(getActivity().getBaseContext()), data.getId());
+        if (i > 0) {
+            DataAccount data = (DataAccount) adapter.getItem(i - 1);
+            controllerAccount.setAccount(ManagerSession.getToken(getActivity().getBaseContext()), data.getId());
+        }
     }
 
     @Override
@@ -137,5 +141,13 @@ public class FragmentAccounts extends BaseFragment implements OnDataReadListener
             }
         });
         return listView;
+    }
+
+    @Override
+    public void onLock() {
+        ((ActivityMain) getActivity()).getLockReceiver().destroy();
+
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, new FragmentSignIn()).commit();
+
     }
 }
---
+++ mobile/PassMatters/app/src/main/java/eu/asyncro/passmatters/receivers/LockReceiver.java
@@ -0,0 +1,46 @@
+package eu.asyncro.passmatters.receivers;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+
+import eu.asyncro.passmatters.interfaces.OnLockListener;
+
+/**
+ * Created by ahuskano on 1/10/2015.
+ */
+public class LockReceiver {
+
+    public static final String KEY = LockReceiver.class.getPackage().getName() + ".ACTION";
+    private Activity activity;
+
+    private OnLockListener onLockListener = new OnLockListener() {
+        @Override
+        public void onLock() {
+            if (activity != null) activity.finish();
+        }
+    };
+
+    BroadcastReceiver onLockReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(LockReceiver.KEY))
+                onLockListener.onLock();
+        }
+    };
+
+    public LockReceiver(Activity activity, OnLockListener onLockListener) {
+        this.activity = activity;
+        this.onLockListener = onLockListener;
+        LocalBroadcastManager.getInstance(activity).registerReceiver(onLockReceiver, new IntentFilter(LockReceiver.KEY));
+    }
+
+    public void destroy() {
+        LocalBroadcastManager.getInstance(activity).unregisterReceiver(onLockReceiver);
+    }
+
+}

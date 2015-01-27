27968c2a66434b8ba653b4a2807c8ea265bbe63d
Milan Pavlović <bluraymimi@gmail.com>
2015-01-27 19:05:35 +0100

    Removed test comments, changed trayicon

--- desktop/PassMatters/src/eu/asyncro/passmatters/main/Application.java
+++ desktop/PassMatters/src/eu/asyncro/passmatters/main/Application.java
@@ -1,7 +1,7 @@
 package eu.asyncro.passmatters.main;
 
 import eu.asyncro.passmatters.main.controller.MainController;
-import eu.asyncro.passmatters.util.FormFiller;
+import eu.asyncro.passmatters.util.Messenger;
 
 /**
  * This is application entry point class with main method.
@@ -10,7 +10,7 @@ import eu.asyncro.passmatters.util.FormFiller;
 public class Application {
     
     /**
-     * Main. Starts the application by instantiating 
+     * The main method. Starts the application by instantiating 
      * MainController class and calling .startApp() method.
      * @param args the command line arguments
      */
@@ -18,43 +18,14 @@ public class Application {
         
         try {
             
+            
             MainController maincontroller = new MainController();
             maincontroller.startApp();
-            
-            /*
-            FormFiller ff = new FormFiller();
-            boolean res = ff.fillFocusedForm("1q2w3e");
-            if(res) System.out.println("OK");
-            else System.out.println("FALSE");
-            System.exit(0);
-            */
-            
-            //System.out.println("gotovo");
-            
-            
-            /*
-            System.out.println("login...");
-            Hashtable<String, String> params = new Hashtable<>();
-            params.put("username","admin");
-            params.put("password", "admin");
-            Client client = new HTTPClient("http://178.62.212.164/api/login", Client.REQUEST_POST, params);
-            client.sendRequest();
-            */
-
-            /*
-            SenderInterface s = new SSLTCPSender();
-            ConnecControl cc = new TCPSCC(s);
-            */
-
-            /*
-            ConnectionController connectionController = new TCPSocketConnectionController();
-            connectionController.openConnection();
-            connectionController.sendData("{\"step\":1,\"token\":\"e4d64c7d0dcf3996eac73cb0e5a654766fff0dcc\"}");
-            connectionController.closeConnection();
-            */        
+           
 
         } catch (Exception ex) {
             ex.printStackTrace();
+            Messenger.showErrorMessage(ex.getMessage(), null);
         }   
     }
 }
